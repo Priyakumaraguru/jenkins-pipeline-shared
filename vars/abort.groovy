d25e7750e72326f11595b236299d281e09d09d39
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2019-12-17 11:54:47 +0530

    Update abort.groovy

--- vars/abort.groovy
+++ vars/abort.groovy
@@ -1,4 +1,3 @@
-
 import hudson.model.Result
 import hudson.model.Run
 import jenkins.model.CauseOfInterruption.UserInterruption
@@ -24,28 +23,3 @@ def call(body) {
         previousBuild = previousBuild.getPreviousBuildInProgress()
     }
 }
-
-
-
-/*import hudson.model.Result
-import hudson.model.Run
-import jenkins.model.CauseOfInterruption.UserInterruption
-options{
-timeout(0.9)
-}
-def call(body) {
-    // https://stackoverflow.com/a/49901413/4763512
-    Run previousBuild = currentBuild.rawBuild.getPreviousBuildInProgress()
-    while (previousBuild != null) {
-        if (previousBuild.isInProgress()) {
-            def executor = previousBuild.getExecutor()
-            if (executor != null) {
-                echo ">> Aborting older build #${previousBuild.number}."
-                executor.interrupt(Result.ABORTED, new UserInterruption(
-                    "Aborted by newer build #${currentBuild.number}."
-                ))
-            }
-        }
-        previousBuild = previousBuild.getPreviousBuildInProgress()
-    }
-}*/
