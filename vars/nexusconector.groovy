8048a1ca9e0ad7ccc5d7eab348f2204cb2948d5e
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-01-24 08:11:28 +0530

    Update nexusconector.groovy

--- vars/nexusconector.groovy
+++ vars/nexusconector.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-sh 'curl -X POST -i -H  "Accept: application/json" -H "Content-Type: application/json" -trace-ascii -d "@repo.json" -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories"'
+sh 'curl -X POST -i -H  "Accept: application/json" -H "Content-Type: application/json" -trace-ascii -d "@https://github.com/Priyakumaraguru/nexus/blob/master/repo.json" -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories"'
 }
